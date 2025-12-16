e683a9b1be220acbbe917d54237b019b707870d5
Žák VR 15 <zak.vr.15@sferapardubice.eu>
2025-12-16 16:30:03 +0100

    s

--- project/uvod.gd
+++ project/uvod.gd
@@ -2,4 +2,4 @@ extends Sprite2D
 
 
 func _on_klik_bez_barvy_pressed() -> void:
-	get_tree().change_scene_to_file("res://Vanoce.tscn")
+	get_tree().change_scene_to_file("res://Vanoce1.tscn")
--- project/vanoce.gd
+++ project/vanoce.gd
@@ -11,4 +11,5 @@ func _on_měsíc_button_up() -> void:
 	print("JSEM ZMACKNUTEJ")
 	$"měsíc2".visible = true
 	# Replace with function body.
-	get_tree().change_scene_to_file("res://Vanoce1.tscn")
+func _on_klik_bez_barvy_pressed() -> void:
+	get_tree().change_scene_to_file("res://Vanoce2.tscn")
